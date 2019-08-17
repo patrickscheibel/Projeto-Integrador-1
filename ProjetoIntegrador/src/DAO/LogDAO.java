b697844f43431c19fb697a8394b323988c3901b6
GuilhermeConti <guilherme.conti@universo.univates.br>
2019-08-17 16:16:36 -0300

    Atualizacao.entidade.log

---
+++ ProjetoIntegrador/src/DAO/LogDAO.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Entidade.Log;
+import Entidade.Usuario;
+import Hibernate.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Scheibel
+ */
+public class LogDAO {
+    
+//    private Usuario usuario = new Usuario(); 
+    
+    public void SalvarLog(String descricao,String erro, String dados){
+    Session sessao = null;
+    try {
+        sessao = HibernateUtil.getSessionFactory().openSession();
+        Transaction t = sessao.beginTransaction();
+        
+        
+           
+            Log logSalvar = new Log();
+            
+            logSalvar.setData(logSalvar.dataAtual());
+            logSalvar.setDescricao(descricao);
+            logSalvar.setDescricao(erro);
+            logSalvar.setDados(dados);
+            
+            sessao.save(logSalvar);                     
+            t.commit();    
+        
+          
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            sessao.close();
+        }
+
+    }
+        
+    //Consulta para utilizar no relatorio
+    public List<Log> ConsultarTodos() {
+    List resultado = null;
+
+        try {
+            Session sessao = HibernateUtil.getSessionFactory().openSession();
+            sessao.beginTransaction();
+
+            org.hibernate.Query q = sessao.createQuery("from Log");
+            resultado = q.list();
+
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return resultado;
+    }           
+
+    
+}
--- ProjetoIntegrador/src/Entidade/Log.java
+++ ProjetoIntegrador/src/Entidade/Log.java
@@ -5,10 +5,102 @@
  */
 package Entidade;
 
+import com.sun.istack.internal.NotNull;
+import java.util.Calendar;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 /**
  *
- * @author Scheibel
+ * @author conti
  */
+@Entity
+@Table(name = "log")
 public class Log {
     
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @NotNull()
+    @Column(name = "data")
+    private Calendar data;
+    
+    @NotNull()
+    @Column(name = "descricao")
+    private String descricao;
+    
+    @NotNull()
+    @Column(name = "erro")
+    private String erro;
+    
+    @NotNull()
+    @Column(name = "dados")
+    private String dados;
+    
+    public Log(){}
+
+    public Log(int id, Calendar data, String descricao, String erro, String dados) {
+        this.id = id;
+        this.data = data;
+        this.descricao = descricao;
+        this.erro = erro;
+        this.dados = dados;
+    }
+
+    @Override
+    public String toString() {
+        return "Log{" + "id=" + id + ", data=" + data + ", descricao=" + descricao + ", erro=" + erro + ", dados=" + dados + '}';
+    }
+    
+    public Calendar dataAtual(){
+        return Calendar.getInstance();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+        public Calendar getData() {
+        return data;
+    }
+
+    public void setData(Calendar data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    public String getErro() {
+        return erro;
+    }
+
+    public void setErro(String erro) {
+        this.erro = erro;
+    }
+       
+
+    public String getDados() {
+        return dados;
+    }
+
+    public void setDados(String dados) {
+        this.dados = dados;
+    }
 }
