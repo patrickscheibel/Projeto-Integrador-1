3a8d3e0910dfecf016aee78d56565e4a65867138
patrick.scheibel <47672652+TheNYPsycho@users.noreply.github.com>
2019-08-28 16:30:07 -0300

    Update Criacao.sql

--- ProjetoIntegrador/src/DataBase/Criacao.sql
+++ ProjetoIntegrador/src/DataBase/Criacao.sql
@@ -1,16 +1,33 @@
+--Repensar
 create table formula(
     id serial PRIMARY KEY,
     nome varchar(100) not null,
     descricao varchar(100) not null
 );
+
+create table cor_material(
+    id serial PRIMARY KEY,
+    descricao varchar(100) not null,
+    radiacao_minima decimal(10,2) not null,
+    radiacao_maxima decimal(10,2) not null
+);
+
+create table tipo_material(
+     id serial PRIMARY KEY,
+     descricao varchar(100) not null
+);
 					  
 create table material(
     id serial PRIMARY KEY,
+    tipo_id integer not null,
     descricao varchar(100) not null,
-    tipo varchar(100),
-    aplicacao varchar(100),
-    tamanho varchar(20), 
-    preco decimal(10,2) not null
+    cor_id integer not null,    
+    emissividade decimal(10,2) not null,
+    conducao_termica decimal(10,2) not null,
+    --preco decimal(10,2),
+    preco money,
+    FOREIGN KEY (cor_id) REFERENCES cor_material(id),
+    FOREIGN KEY (tipo_id) REFERENCES tipo_material(id)
 );
 
 create table usuario(
